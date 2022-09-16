f7acb5eef315de9d25175880a3e47bd93a2588cf
grealesguti <g.realesguti@gmail.com>
2022-09-16 17:11:27 +0200

    add: all Job Submission file loop

--- SubFiles/AllJobSubmission.sh
+++ SubFiles/AllJobSubmission.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 BASEDIR=/storage/af/user/greales/simG4/Repov2
-condor_submit $BASEDIR/SubFiles/SubmissionFileGC1FLResinMuon.sub
-condor_submit $BASEDIR/SubFiles/SubmissionFileGC13mmResinMuon.sub
-condor_submit $BASEDIR/SubFiles/SubmissionFileGC3Muon.sub
-condor_submit $BASEDIR/SubFiles/SubmissionFileGC1FLResin.sub
-condor_submit $BASEDIR/SubFiles/SubmissionFileGC13mmResin.sub
-condor_submit $BASEDIR/SubFiles/SubmissionFileGC3.sub
+for((i=1;i<=10;i++))
+do
+    condor_submit $BASEDIR/SubFiles/SubmissionFileGC1FLResinMuon.sub
+    condor_submit $BASEDIR/SubFiles/SubmissionFileGC13mmResinMuon.sub
+    condor_submit $BASEDIR/SubFiles/SubmissionFileGC3Muon.sub
+    condor_submit $BASEDIR/SubFiles/SubmissionFileGC1FLResin.sub
+    condor_submit $BASEDIR/SubFiles/SubmissionFileGC13mmResin.sub
+    condor_submit $BASEDIR/SubFiles/SubmissionFileGC3.sub
+done

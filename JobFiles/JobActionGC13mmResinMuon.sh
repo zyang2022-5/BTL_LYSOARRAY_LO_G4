bdacc13f241584eac2f511d66626f364f7345b26
grealesguti <g.realesguti@gmail.com>
2022-09-16 16:36:40 +0200

    add: Submission sh all at once for muon
    All Submission and Job files in separate folders.
    Sh file to launch all Muon jobs at once.

--- JobFiles/JobActionGC13mmResinMuon.sh
+++ JobFiles/JobActionGC13mmResinMuon.sh
@@ -6,7 +6,7 @@ BASEDIR="/storage/af/user/greales/simG4/Repov2/"
 SimName="sim"    # Name of the G4 executable
 MacroName="run.mac"     # Name of the macro file to run with the G4 executable
 SimArgs="-m"            # Arguments for the G4 executable (any)
-OutPut="Name"   # Name of the Root output files from the G4 executable 
+OutPut="GC1_3mmResin_Muon_"   # Name of the Root output files from the G4 executable 
 
 while getopts "a:n:s:" arg; do
     case $arg in
@@ -27,4 +27,4 @@ echo "*** Sourcing G4:"
 
 echo "Running Simulation" 
 cd $BASEDIR
-./$SimName -o $OutName -GeomConfig 3 -m run.mac -Muon
+./$SimName -o $OutName -GeomConfig 1 RESIN_W 1.6 -m run.mac -Muon
---
+++ SubFiles/AllJobSubmission.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+condor_submit SubmissionFileGC1FLResinMuon.sub
+condor_submit SubmissionFileGC13mmResinMuon.sub
+condor_submit SubmissionFileGC3Muon.sub

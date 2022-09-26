918d7d2f993c6d56109e78caed9a056990ddd133
Guillermo <g.realesguti@gmail.com>
2022-09-26 11:05:28 +0200

    add: Separate G4sim Class for TierII
    New class without loading the visualization routines.

---
+++ src_G4/G4simTierII.cc
@@ -0,0 +1,52 @@
+#include "G4sim.hh"
+
+G4simulation::G4simulationNOVIS(int mainargc,char** mainargv, G4int Onode , G4int Znode , G4double* radp)
+{
+  // Set Up / Run  Initialization
+    G4RunManager *runManager = new G4RunManager();
+    ArgInp = new MyG4Args(mainargc, mainargv);
+    
+    if(radp == NULL){
+        ArgInp->DefaultRadiusVect();
+    }else{
+        ArgInp->SetRadiusVect(radp,Onode,Znode);
+    }
+
+    runManager -> SetUserInitialization(new MyDetectorConstruction(ArgInp)); /*Define geometry*/
+    runManager -> SetUserInitialization(new MyPhysicsList()); /*Define physics*/
+    runManager -> SetUserInitialization(new MyActionInitialization(ArgInp)); /*Define actions*/
+
+    runManager -> Initialize();
+
+///////* Vis -> Visualizer*//////
+//    G4UIExecutive *ui = 0; // ui initialization
+    // Show *user interface only if we do not use a macro, or it will be inefficient    
+//    if (ArgInp->GetVis() ==1) // argc only one option introduced
+//    {
+//        ui = new G4UIExecutive(mainargc,mainargv); // ui definition-declaration
+//    }   
+    /* Vis -> Visualizer*/  // Initialization
+//    G4VisManager *visManager = new G4VisExecutive();
+//    visManager->Initialize();
+///////* Vis -> Visualizer*//////
+
+    G4UImanager *UImanager = G4UImanager::GetUIpointer();
+    if(ArgInp->GetVis() ==1)
+    {
+//        UImanager->ApplyCommand("/control/execute vis.mac");
+//        ui->SessionStart();     ///////* Vis -> Visualizer*//////
+    }
+    else
+    {
+                    G4String command = "/control/execute ";  
+                    G4String fileName = ArgInp->GetMacName();
+                    G4int nrep = ArgInp->Getnrep();
+                    for (int i = 1; i <= nrep; i=i+1){
+                        UImanager->ApplyCommand(command+fileName);
+                    }
+        }
+
+}
+G4simulation::~G4simulation()
+{}
+
---
+++ src_G4/G4simTierII.hh
@@ -0,0 +1,38 @@
+#ifndef G4SIM_HH
+#define G4SIM_HH
+
+#include <iostream>
+#include "G4RunManager.hh" /* Run */
+#include "G4UImanager.hh"
+
+///////* Vis -> Visualizer*//////
+//#include "G4VisManager.hh"      /* Vis -> Visualizer*/
+//#include "G4VisExecutive.hh"    /* Vis -> Visualizer*/
+//#include "G4UIExecutive.hh"     /* Vis -> Visualizer*/
+///////* Vis -> Visualizer*//////
+/*Simulation created files*/
+#include "construction.hh"
+#include "physics.hh"
+#include "action.hh"
+#include <cstdlib>
+#include "G4Args.hh"
+#include <unistd.h> /* for exit() */
+
+class G4simulationNOVIS 
+{
+
+public:
+    G4simulationNOVIS(int, char**, G4int Onode = 5, G4int Znode = 1 , G4double* radp = NULL);
+    ~G4simulationNOVIS();
+    G4double GetAvgLO(){}
+    double GetLO_avg(int runid){return ArgInp->GetLOAvg(runid);}
+    double GetLO_std(int runid){return ArgInp->GetLOStd(runid);}
+    double GetLD_avg(int runid){return ArgInp->GetLDAvg(runid);}
+    double GetLD_std(int runid){return ArgInp->GetLDStd(runid);}
+
+private:
+
+    MyG4Args *ArgInp;
+};    
+
+#endif

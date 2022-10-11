ab38ce5550321ba7f7eb388f2490d7c2120ba1c0
grealesguti <g.realesguti@gmail.com>
2022-10-11 13:58:36 +0200

    fix: Modifs from TierII

--- SubFiles/SingleJobSubmission.sh
+++ SubFiles/SingleJobSubmission.sh
@@ -2,12 +2,12 @@
 BASEDIR=/storage/af/user/greales/simG4/BTL_LYSOARRAY_LO_G4
 MAXINCR=125
 MININCR=75
-STEPS=50
+STEPS=2
 DIST=$(( ($MAXINCR - $MININCR ) / $STEPS ))
 echo $DIST
 
 ARGSUB=$(( $MININCR - $DIST ))
-for((i=1;i<=$(STEPS);i++))
+for((i=1;i<=$STEPS;i++))
 do
     ARGSUB=$(( $ARGSUB + $DIST ))
     condor_submit $BASEDIR/SubFiles/SubmissionFileGC1FLResinMuonIncr.sub
@@ -18,3 +18,4 @@ do
 #    condor_submit $BASEDIR/SubFiles/SubmissionFileGC13mmResin.sub
 #    condor_submit $BASEDIR/SubFiles/SubmissionFileGC3.sub
 done
+

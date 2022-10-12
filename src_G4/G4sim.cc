83c8def0a65169480163a6d5a85acb99f9891cd8
grealesguti <g.realesguti@gmail.com>
2022-10-12 12:57:10 +0200

    fix: issue with incr commands
    the -incr was removed from G4sim and the if conditions were not allowing
    the results to get into -incrS or -incrV

--- src_G4/G4sim.cc
+++ src_G4/G4sim.cc
@@ -6,8 +6,8 @@ G4simulation::G4simulation(int mainargc,char** mainargv, G4int Onode , G4int Zno
     G4RunManager *runManager = new G4RunManager();
     ArgInp = new MyG4Args(mainargc, mainargv);
     
-    if(radp == NULL){
-        if(ArgInp->Getrad2Y() == 1 && ArgInp->GetIncr() == 0){
+        if(radp == NULL){
+        if(ArgInp->Getrad2Y() == 1 && ArgInp->GetIncr() == 0 && ArgInp->GetIncrS() == 0 && ArgInp->GetIncrV() == 0){
             G4cout<< " !!! WARNING !!! rad2Y set to 1 but no arguments were passed, set to 1. " <<G4endl;  
             G4double radinit[2]={1,2};
             //radinit[0]=1;radinit[1]=1;
@@ -18,6 +18,22 @@ G4simulation::G4simulation(int mainargc,char** mainargv, G4int Onode , G4int Zno
         }else if(ArgInp->GetIncr() > 0){
             G4double radinit[2]={1,ArgInp->GetIncr()};
             //radinit[0]=1;radinit[1]=1;
+            G4double* radones=radinit;
+            ArgInp->DefaultRadiusVect();
+            ArgInp->SetCoordVect();
+            ArgInp->SetYVect(radones); 
+         }else if(ArgInp->GetIncrS() > 0){
+            G4double radinit[2]={ArgInp->GetIncrS(),1};
+            //radinit[0]=1;radinit[1]=1;
+            G4double* radones=radinit;
+            ArgInp->DefaultRadiusVect();
+            ArgInp->SetCoordVect();
+            ArgInp->SetYVect(radones); 
+        }else if(ArgInp->GetIncrV() > 0){
+            G4double SiPMIncr = 1-1*(ArgInp->GetIncrV()-1);
+            G4double radinit[2];//={SiPMIncr,ArgInp->GetIncrV()};
+            radinit[0]=SiPMIncr;radinit[1]=ArgInp->GetIncrV();
+            G4cout<<"### Rad SiPM: "<< radinit[0]<< " Middle: "<< radinit[1]<<G4endl; 
             G4double* radones=radinit;
             ArgInp->DefaultRadiusVect();
             ArgInp->SetCoordVect();
@@ -63,6 +79,13 @@ G4simulation::G4simulation(int mainargc,char** mainargv, G4int Onode , G4int Zno
     {
         UImanager->ApplyCommand("/control/execute vis.mac");
         ui->SessionStart();     ///////* Vis -> Visualizer*//////
+    }else if(ArgInp->GetRunEvt()>0)
+    {
+        int EvtRun = ArgInp->GetRunEvt();
+        G4String command;  
+        command = "/run/beamOn "+std::to_string(EvtRun); 
+        G4cout<< "Running through C++ -- "<<command << G4endl;
+        UImanager->ApplyCommand(command);
     }
     else
     {
--- src_G4/G4simTierII.cc
+++ src_G4/G4simTierII.cc
@@ -9,8 +9,8 @@ G4simulationNOVIS::G4simulationNOVIS(int mainargc,char** mainargv, G4int Onode ,
 
     ArgInp = new MyG4Args(mainargc, mainargv);
     
-    if(radp == NULL){
-        if(ArgInp->Getrad2Y() == 1 && ArgInp->GetIncr() == 0){
+        if(radp == NULL){
+        if(ArgInp->Getrad2Y() == 1 && ArgInp->GetIncr() == 0 && ArgInp->GetIncrS() == 0 && ArgInp->GetIncrV() == 0){
             G4cout<< " !!! WARNING !!! rad2Y set to 1 but no arguments were passed, set to 1. " <<G4endl;  
             G4double radinit[2]={1,2};
             //radinit[0]=1;radinit[1]=1;
@@ -18,7 +18,14 @@ G4simulationNOVIS::G4simulationNOVIS(int mainargc,char** mainargv, G4int Onode ,
             ArgInp->DefaultRadiusVect();
             ArgInp->SetCoordVect();
             ArgInp->SetYVect(radones);      
-        }else if(ArgInp->GetIncrS() > 0){
+        }else if(ArgInp->GetIncr() > 0){
+            G4double radinit[2]={1,ArgInp->GetIncr()};
+            //radinit[0]=1;radinit[1]=1;
+            G4double* radones=radinit;
+            ArgInp->DefaultRadiusVect();
+            ArgInp->SetCoordVect();
+            ArgInp->SetYVect(radones); 
+         }else if(ArgInp->GetIncrS() > 0){
             G4double radinit[2]={ArgInp->GetIncrS(),1};
             //radinit[0]=1;radinit[1]=1;
             G4double* radones=radinit;
@@ -27,8 +34,9 @@ G4simulationNOVIS::G4simulationNOVIS(int mainargc,char** mainargv, G4int Onode ,
             ArgInp->SetYVect(radones); 
         }else if(ArgInp->GetIncrV() > 0){
             G4double SiPMIncr = 1-1*(ArgInp->GetIncrV()-1);
-            G4double radinit[2]={SiPMIncr,ArgInp->GetIncrV()};
-            //radinit[0]=1;radinit[1]=1;
+            G4double radinit[2];//={SiPMIncr,ArgInp->GetIncrV()};
+            radinit[0]=SiPMIncr;radinit[1]=ArgInp->GetIncrV();
+            G4cout<<"### Rad SiPM: "<< radinit[0]<< " Middle: "<< radinit[1]<<G4endl; 
             G4double* radones=radinit;
             ArgInp->DefaultRadiusVect();
             ArgInp->SetCoordVect();
